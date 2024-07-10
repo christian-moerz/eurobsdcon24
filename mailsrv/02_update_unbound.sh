5eb68b3e88a77abfb6385852c4b790fe1f290151
Christian Moerz <chris@ny-central.org>
2024-07-10 09:10:28 +0200

    correction on dns path

--- mailsrv/02_update_unbound.sh
+++ mailsrv/02_update_unbound.sh
@@ -5,9 +5,15 @@
 install_key()
 {
     if [ -e $1.lab.dns ]; then
-	cat $1.lab.dns >> /usr/local/etc/unbound/$1.zone
+	# make sure to remove any pre-existing keys
+	sed -i '' '/_default/d' /usr/local/etc/unbound/$1.zone
+	sed -i '' '/"p=/d' /usr/local/etc/unbound/$1.zone
+	sed -i '' '/DKIM key/d' /usr/local/etc/unbound/$1.zone
+	
+	cat $1.dns >> /usr/local/etc/unbound/$1.zone
 	service unbound reload
+	echo "$1 DKIM key installed."
     fi
 }
 
-install_key ny-central
+install_key ny-central.lab
